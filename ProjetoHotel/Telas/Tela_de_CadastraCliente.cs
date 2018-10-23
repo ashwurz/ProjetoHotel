471e1e91391d89fe58d7f526f014243a3bd09687
ViniciusLOPS <viniciuslops@hotmail.com>
2018-10-23 16:08:09 -0300

    Pessoa implementada em LES, sendo cadastrada pelo FUncionario (incompleto)

---
+++ ProjetoHotel/Telas/Tela_de_CadastraCliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoHotel.Telas
+{
+    public partial class Tela_de_CadastraCliente : Form
+    {
+        public Tela_de_CadastraCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
