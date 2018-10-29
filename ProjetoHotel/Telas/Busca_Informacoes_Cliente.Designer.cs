07e03af87e8b3365838b5eb0f7f6f7d0bc17cde7
ViniciusLOPS <viniciuslops@hotmail.com>
2018-10-28 23:53:00 -0300

    so pra dar pull

--- ProjetoHotel/Telas/Busca_Informacoes_Cliente.Designer.cs
+++ ProjetoHotel/Telas/Busca_Informacoes_Cliente.Designer.cs
@@ -73,7 +73,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Nome:";
             // 
@@ -81,7 +81,7 @@
             // 
             this.txtNome.Location = new System.Drawing.Point(83, 68);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(340, 20);
+            this.txtNome.Size = new System.Drawing.Size(340, 21);
             this.txtNome.TabIndex = 3;
             // 
             // label2
@@ -89,7 +89,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Sobrenome:";
             // 
@@ -97,7 +97,7 @@
             // 
             this.txtSobrenome.Location = new System.Drawing.Point(83, 116);
             this.txtSobrenome.Name = "txtSobrenome";
-            this.txtSobrenome.Size = new System.Drawing.Size(340, 20);
+            this.txtSobrenome.Size = new System.Drawing.Size(340, 21);
             this.txtSobrenome.TabIndex = 5;
             // 
             // label3
@@ -105,7 +105,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 169);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "ID:";
             // 
@@ -113,7 +113,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(83, 166);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(340, 20);
+            this.textBox1.Size = new System.Drawing.Size(340, 21);
             this.textBox1.TabIndex = 7;
             // 
             // btnBuscar
@@ -218,6 +218,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.labelCliente);
             this.Controls.Add(this.panelResult);
+            this.Font = new System.Drawing.Font("Script MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Busca_Informacoes_Cliente";
